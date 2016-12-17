6b430d36b270db91f447cb4032d07f4f1fb8ebd6
Arnoud Aarnoudse <justinaarnoudse@hotmail.com>
2016-12-17 02:12:11 +0100

    Cleaning up some code and packages

--- General/src/main/java/screencapture/xuggler/ScreenCaptureXuggler.java
+++ General/src/main/java/screencapture/xuggler/ScreenCaptureXuggler.java
@@ -1,4 +1,5 @@
-package screencapture;
+// http://stackoverflow.com/questions/5840455/capture-screen-with-mouse-pointer-using-xuggler
+package screencapture.xuggler;
 
 import java.awt.AWTException;
 import java.awt.Dimension;
@@ -19,8 +20,8 @@ import com.xuggle.xuggler.ICodec;
 
 public class ScreenCaptureXuggler {
 
-    private static final double FRAME_RATE = 5;
-    private static final int SECONDS_TO_RUN_FOR = 120;
+    private static final double FRAME_RATE = 15;
+    private static final int SECONDS_TO_RUN_FOR = 10;
     private static final String OUTPUT_FILENAME = "E:\\Netbeans Projects\\General\\General\\src\\main\\resources\\media\\XugglerRecording.mp4";
     private static Dimension screenBounds;
     public static Image m_MouseIcon = null;
@@ -33,15 +34,9 @@ public class ScreenCaptureXuggler {
 
         // We tell it we're going to add one video stream, with id 0,
         // at position 0, and that it will have a fixed frame rate of FRAME_RATE.
-        writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_MPEG4, screenBounds.width / 2, screenBounds.height / 2);
+        writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_MPEG4, screenBounds.width, screenBounds.height);
 
         long startTime = System.nanoTime();
-        /**
-        try {
-            m_MouseIcon = ImageIO.read(ScreenCaptureXuggler.class.getResource("resource/captionMouseIcon.png"));
-        } catch (IOException e) {
-            System.out.println("Error: " + e);
-        } */
 
         for (int index = 0; index < SECONDS_TO_RUN_FOR * FRAME_RATE; index++) {
 
@@ -53,14 +48,6 @@ public class ScreenCaptureXuggler {
 
             // encode the image to stream #0
             writer.encodeVideo(0, bgrScreen, System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
-
-            // sleep for frame rate milliseconds
-            try {
-                Thread.sleep((long) (1000 / FRAME_RATE));
-            } catch (InterruptedException e) {
-                // ignore
-            }
-
         }
 
         // tell the writer to close and write the trailer if  needed
