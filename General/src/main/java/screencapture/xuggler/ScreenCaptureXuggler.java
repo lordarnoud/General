02c84a87e139ee0d6f7b6aa31d3a3ae4790fa9e2
Arnoud Aarnoudse <justinaarnoudse@hotmail.com>
2016-12-20 21:46:11 +0100

    Working version of Xuggler including mouse capture

--- General/src/main/java/screencapture/xuggler/ScreenCaptureXuggler.java
+++ General/src/main/java/screencapture/xuggler/ScreenCaptureXuggler.java
@@ -1,76 +1,129 @@
 // http://stackoverflow.com/questions/5840455/capture-screen-with-mouse-pointer-using-xuggler
 package screencapture.xuggler;
 
+import com.xuggle.mediatool.IMediaWriter;
+import com.xuggle.mediatool.ToolFactory;
+import com.xuggle.xuggler.ICodec;
 import java.awt.AWTException;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
-
 import javax.imageio.ImageIO;
-
-import com.xuggle.mediatool.IMediaWriter;
-import com.xuggle.mediatool.ToolFactory;
-import com.xuggle.xuggler.ICodec;
 
 public class ScreenCaptureXuggler {
 
-    private static final double FRAME_RATE = 15;
-    private static final int SECONDS_TO_RUN_FOR = 10;
-    private static final String OUTPUT_FILENAME = "E:\\Netbeans Projects\\General\\General\\src\\main\\resources\\media\\XugglerRecording.mp4";
+    private static final String OUTPUT = "E:\\Netbeans Projects\\General\\General\\src\\main\\resources\\media\\xuggle.mp4";
+    private static final String CURSOR = "/media/cursor.png";
     private static Dimension screenBounds;
     public static Image m_MouseIcon = null;
 
+    /**
+     * This will record a screen for the given time.
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        // let's make a IMediaWriter to write the file.
-        final IMediaWriter writer = ToolFactory.makeWriter(OUTPUT_FILENAME);
+
+        // Make a IMediaWriter to write the file.
+        final IMediaWriter writer = ToolFactory.makeWriter(OUTPUT);
+        start(writer);
+    }
+
+    /**
+     * Start recording the screen.
+     *
+     * @param writer
+     */
+    private static void start(IMediaWriter writer) {
 
         screenBounds = Toolkit.getDefaultToolkit().getScreenSize();
 
-        // We tell it we're going to add one video stream, with id 0,
-        // at position 0, and that it will have a fixed frame rate of FRAME_RATE.
+        // Add one video stream, with id 0,
+        // at position 0, with a fixed frame rate of FRAME_RATE.
         writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_MPEG4, screenBounds.width, screenBounds.height);
 
+        // Save the start time because it will be used to encode the video properly.
         long startTime = System.nanoTime();
 
-        for (int index = 0; index < SECONDS_TO_RUN_FOR * FRAME_RATE; index++) {
-
-            // take the screen shot
-            BufferedImage screen = getDesktopScreenshot();
-
-            // convert to the right image type
-            BufferedImage bgrScreen = convertToType(screen, BufferedImage.TYPE_3BYTE_BGR);
-
-            // encode the image to stream #0
-            writer.encodeVideo(0, bgrScreen, System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
+        // Attempt to load the mouse pointer.
+        // TODO - add a second pointer for when the mouse was pressed.
+        try {
+            URL resource = ScreenCaptureXuggler.class.getResource(CURSOR);
+            m_MouseIcon = ImageIO.read(resource);
+        } catch (IOException e1) {
+            System.out.println("Error: " + e1);
         }
 
-        // tell the writer to close and write the trailer if  needed
-        writer.close();
+        // Set the amount of time for the recorder to record.
+        // TODO - replace with a start and stop method, no timer needed.
+        long endTime = System.currentTimeMillis() + 5000;
 
+        while (System.currentTimeMillis() < endTime) {
+            // take the screenshot
+            BufferedImage screenshot = getDesktopScreenshot();
+
+            // Convert the screenshot to the right image type
+            BufferedImage bgrScreen = convertToType(screenshot, BufferedImage.TYPE_3BYTE_BGR);
+
+            // Draw the mouse pointer in the screen.
+            Graphics2D bGr = bgrScreen.createGraphics();
+            Point location = MouseInfo.getPointerInfo().getLocation();
+            bGr.drawImage(m_MouseIcon, location.x, location.y, null);
+            bGr.dispose();
+
+            // Encode the image to stream #0.
+            writer.encodeVideo(0, bgrScreen, System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
+        }
+        stop(writer);
     }
 
+    /**
+     * Stop recording.
+     *
+     * @param writer the mediaWriter that needs to be stopped.
+     */
+    private static void stop(IMediaWriter writer) {
+        // Close the writer.
+        writer.close();
+    }
+
+    /**
+     * Convert the screenshot to the right image type
+     *
+     * @param sourceImage the source image.
+     * @param targetType the type of image it needs to become
+     *
+     * @return the converted image.
+     */
     public static BufferedImage convertToType(BufferedImage sourceImage, int targetType) {
         BufferedImage image;
 
-        // if the source image is already the target type, return the source image
+        // Ff the source image is already the target type, return the source image
         if (sourceImage.getType() == targetType) {
             image = sourceImage;
-        } // otherwise create a new image of the target type and draw the new image
+        } // Otherwise create a new image of the target type and draw the new image
         else {
             image = new BufferedImage(sourceImage.getWidth(), sourceImage.getHeight(), targetType);
             image.getGraphics().drawImage(sourceImage, 0, 0, null);
         }
 
         return image;
-
     }
 
+    /**
+     * Take a screenshot of the desktop.
+     *
+     * @return the screenshot
+     */
     private static BufferedImage getDesktopScreenshot() {
         try {
             Robot robot = new Robot();
